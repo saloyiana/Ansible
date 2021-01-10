ff5c7914c3999a23939953bbd6a5a6db75c3d716
sara <saronah1099@gmail.com>
2021-01-10 17:57:27 +0300

    install ansible

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install python3 -y
+sudo apt install python3-pip -y
+sudo pip3 install ansible
+
---
+++ verification.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ansible --version
+ansible localhost -m ping
